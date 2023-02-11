8b9596ca8c7d9403730cd453890e7ca2b8c03e72
zappaciato <58880515+zappaciato@users.noreply.github.com>
2023-02-11 10:20:06 +0100

    Second commmit with Hello World

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+import pywhatkit
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+pywhatkit.text_to_handwriting('''Learning Python from the basics is 
+extremely important. Before starting to learn python,understanding a 
+base language like c is a must and 
+some of the oops concepts.Python program has many modulesand packages, 
+which helps with efficient programming.
+Understanding these modules and 1proper usage of many syntax and libraries is recommended.
+In this article, a few modules and packages are used in the program. 
+Python includes tons of libraries and some of them are quiet intresting''')
